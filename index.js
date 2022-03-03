71387e215e9394833c90cc0b07ef914c9b33ac9b
KavyaReddy732 <kavyareddy.732@gmail.com>
2022-03-03 14:14:14 +0100

    changes

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const fileInput = document.getElementById('file-selector')
 fileInput.addEventListener('change', async (event) => {
     const text = await event.target.files[0].text()
     let trimmed = text.split("\n").map(e => e.trim());
-    console.log(trimmed)
-
-    // let N, M;
-    // let packages = [], dependencies = [];
-    // N = trimmed[0]
-    // console.log(N)
+    let N = trimmed[0];
+    let packages = [], dependencies = [];
+    let M;
+    for (let i = 1; i <= N; i++) {
+        packages.push(trimmed[i])
+        M = trimmed[i + 1]
+        dependencies = trimmed.slice(i + 2)
+    }
+    console.log(N, packages, M, dependencies)
 
 })
