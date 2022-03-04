955adab4142b14cf57f0e326f899d681116b4957
KavyaReddy732 <kavyareddy.732@gmail.com>
2022-03-04 17:58:49 +0100

    modified

--- index.js
+++ index.js
@@ -5,21 +5,16 @@ fileInput.addEventListener('change', async (event) => {
         throw ('the file is empty')
     }
     const trimmed = text.split("\n").map(e => e.trim());
-
-    let N = trimmed[0]; //number of packages to install
-    let packages = [], dependencies = [];
-    let M; // number of dependencies
+    let N = trimmed[0], packages = [], dependencies = [], M; //number of packages to install
 
     for (let i = 1; i <= N; i++) {
         packages.push(trimmed[i].split(','))
         M = trimmed[i + 1]
-        dependencies = trimmed.slice(i + 2)
+        dependencies = trimmed.slice(i + 2).filter(e => e !== '');
     }
 
-    var packageObjects = packages.map(package => ({ package: package[0], version: package[1] })); //name and version of packages to install 
-    console.log(packageObjects)
-
-
+    var packageObjects = packages.map(package => ({ name: package[0], version: package[1] }));//name and version of packages to install
+    console.log(packageObjects, 'packages')
     let result = [];
     let dependencyPairs = [];
 
@@ -30,11 +25,11 @@ fileInput.addEventListener('change', async (event) => {
             while (i < n) {
                 chunks.push(arr.slice(i, i += len));
             }
-            console.log(chunks)
-            const dependencyObjects = chunks.map(dependency => ({ dependency: dependency[0], version: dependency[1] }))
+            const dependencyObjects = chunks.map(dependency => ({ name: dependency[0], version: dependency[1] }))
             return dependencyObjects;
         }
         dependencyPairs.push(splitArrayIntoChunksOfLen(result[i], 2));
     }
+
     console.log(dependencyPairs) // dependencies to install 
 })
